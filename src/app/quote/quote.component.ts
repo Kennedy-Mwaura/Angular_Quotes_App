165c580610ddaca8e82e5ffef8ded705011b9924
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-21 15:49:25 +0300

    added code to add, delete quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -18,6 +18,29 @@ export class QuoteComponent implements OnInit {
     new Quote(2, 'Ken', 'I love technology', new Date(2021, 5, 14), 12,3, false),
   ];
 
+  addNewQuote(quote: any){
+    let quoteLength = this.techQuotes.length;
+    let quoteObject = new Quote(quoteLength+1, quote.name, quote.quote, new Date(quote.date), quote.likes, quote.dislikes, false);
+
+    // quote.id = quoteLength+1;
+    this.techQuotes.push(quoteObject);
+  }
+  
+  deleteTechQuotes(index:number, isDeleted:boolean){
+    if(isDeleted){
+      let toDelete = confirm(`Are you sure you want to delete ${this.techQuotes[index].quote}?`);
+      if(toDelete){
+        this.techQuotes.splice(index, 1);
+      }
+    }
+  }
+
+
+  
+  showQuoteDetails(index: number){
+    this.techQuotes[index].showQuoteDescription = !this.techQuotes[index].showQuoteDescription;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
