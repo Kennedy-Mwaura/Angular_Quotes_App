6798ad890c400359afd2d229f60a3ac4c5fc4293
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-21 15:12:43 +0300

    added code to  pipe to calculate dates elapsed

--- src/app/date-elapsed.pipe.ts
+++ src/app/date-elapsed.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateElapsedPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    // retun difference in days 
+    const today: Date = new Date();
+    const todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInDay = 86400;
+    let dateDifferenceSeconds = dateDifference * 0.001;
+    const dateElapsed = Math.floor(dateDifferenceSeconds / secondsInDay);
+
+    return dateElapsed;
   }
 
 }
